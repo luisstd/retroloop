8c9c54249cc96223f5780cd78468626055b31e08
Luis Steidle <luis@steidle.me>
2022-08-05 10:39:56 +0200

    chore: edit prettier & eslint config

--- pages/index.tsx
+++ pages/index.tsx
@@ -17,8 +17,7 @@ const Home: NextPage = () => (
       </h1>
 
       <p className={styles.description}>
-        Get started by editing{" "}
-        <code className={styles.code}>pages/index.tsx</code>
+        Get started by editing <code className={styles.code}>pages/index.tsx</code>
       </p>
 
       <div className={styles.grid}>
@@ -32,10 +31,7 @@ const Home: NextPage = () => (
           <p>Learn about Next.js in an interactive course with quizzes!</p>
         </a>
 
-        <a
-          href="https://github.com/vercel/next.js/tree/canary/examples"
-          className={styles.card}
-        >
+        <a href="https://github.com/vercel/next.js/tree/canary/examples" className={styles.card}>
           <h2>Examples &rarr;</h2>
           <p>Discover and deploy boilerplate example Next.js projects.</p>
         </a>
