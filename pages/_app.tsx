fad97b49edd61d9a08fb086d91c0b24a469d5bfc
Luis Steidle <luis@steidle.me>
2022-08-12 19:36:40 +0200

    chore: add placeholder homepage

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,8 +1,47 @@
-import '../styles/globals.css'
-import type { AppProps } from 'next/app'
+import { withTRPC } from '@trpc/next'
+import { AppType } from 'next/dist/shared/lib/utils'
+import { AppRouter } from './api/trpc/trpc'
 
-function MyApp({ Component, pageProps }: AppProps) {
+import '../styles/globals.css'
+
+const MyApp: AppType = ({ Component, pageProps }) => {
   return <Component {...pageProps} />
 }
 
-export default MyApp
+function getBaseUrl() {
+  if (typeof window !== 'undefined') {
+    return ''
+  }
+  // reference for vercel.com
+  if (process.env.VERCEL_URL) {
+    return `https://${process.env.VERCEL_URL}`
+  }
+
+  // reference for render.com
+  if (process.env.RENDER_INTERNAL_HOSTNAME) {
+    return `http://${process.env.RENDER_INTERNAL_HOSTNAME}:${process.env.PORT}`
+  }
+
+  // assume localhost
+  return `http://localhost:${process.env.PORT ?? 3000}`
+}
+
+export default withTRPC<AppRouter>({
+  config() {
+    /**
+     * If you want to use SSR, you need to use the server's full URL
+     * @link https://trpc.io/docs/ssr
+     */
+    return {
+      url: `${getBaseUrl()}/api/trpc`,
+      /**
+       * @link https://react-query-v3.tanstack.com/reference/QueryClient
+       */
+      // queryClientConfig: { defaultOptions: { queries: { staleTime: 60 } } },
+    }
+  },
+  /**
+   * @link https://trpc.io/docs/ssr
+   */
+  ssr: true,
+})(MyApp)
