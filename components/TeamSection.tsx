59865bc3070df1aabc037580126156df55aea200
Luis Steidle <luis@steidle.me>
2022-08-19 20:22:15 +0200

    feat: add TeamSection component

---
+++ components/TeamSection.tsx
@@ -0,0 +1,9 @@
+export default function TeamSection() {
+  return (
+    <>
+      <section className='mt-5 border-2 border-black rounded-md'>
+        <h2>TEAM</h2>
+      </section>
+    </>
+  )
+}
