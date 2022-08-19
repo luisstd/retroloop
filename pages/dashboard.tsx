f8b881b0d22c58f045fcef53db62dc357c323934
Luis Steidle <luis@steidle.me>
2022-08-19 20:21:28 +0200

    feat: add dashboard page

---
+++ pages/dashboard.tsx
@@ -0,0 +1,24 @@
+import type { NextPage } from 'next'
+import Head from 'next/head'
+
+import MenuBar from '../components/MenuBar'
+import RetroSection from '../components/RetroSection'
+import TeamSection from '../components/TeamSection'
+
+const Dashboard: NextPage = () => (
+  <div>
+    <Head>
+      <title>Dashboard</title>
+      <meta name='description' content='Agile Retrospectives' />
+      <link rel='icon' href='/favicon.ico' />
+    </Head>
+    <MenuBar />
+    <main className='grid h-screen col-span-2 row-span-2 m-2 '>
+      <RetroSection />
+
+      <TeamSection />
+    </main>
+  </div>
+)
+
+export default Dashboard
