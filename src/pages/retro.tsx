6d6cb0833689098bcb390241ca46c0b49a941a9c
Luis Steidle <luis@steidle.me>
2022-11-18 22:25:37 +0100

    refactor: remove unnecessary prop

--- src/pages/retro.tsx
+++ src/pages/retro.tsx
@@ -1,14 +1,11 @@
 import React from 'react'
 import { NextPage } from 'next'
 import Head from 'next/head'
-import { useRouter } from 'next/router'
 
 import MenuBar from '@/components/MenuBar/MenuBar'
 import RetroView from '@/components/RetroView/RetroView'
 
 const Retro: NextPage = () => {
-  const router = useRouter()
-
   return (
     <div className='flex flex-col items-center'>
       <Head>
@@ -19,7 +16,7 @@ const Retro: NextPage = () => {
 
       <MenuBar />
 
-      <RetroView retroName={router.query.name} />
+      <RetroView />
     </div>
   )
 }
