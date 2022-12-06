3654f8ad9ac1294f219eda88df105372b9653236
Luis Steidle <luis@steidle.me>
2022-12-06 20:22:25 +0100

    refactor: refactor footer links into radix-ui navigation menu

--- src/components/Footer/Footer.tsx
+++ src/components/Footer/Footer.tsx
@@ -1,25 +1,31 @@
+import * as NavigationMenu from '@radix-ui/react-navigation-menu'
+
 export default function Footer() {
   return (
     <>
       <footer className='p-3 m-5 rounded-md dark:border-neutral-200 max-w-screen-2x'>
         <div className='grid place-items-center'>
-          <div>
-            <a
-              href='/privacy'
-              className='p-2 px-2 text-sm transition ease-in-out rounded-md cursor-poiner hover:bg-neutral-100 dark:hover:text-black'
-            >
-              Privacy Policy
-            </a>
+          <NavigationMenu.Root>
+            <NavigationMenu.List className='flex items-center gap-2'>
+              <NavigationMenu.Item className='text-center'>
+                <NavigationMenu.Link
+                  className='p-2 px-2 text-sm transition ease-in-out rounded-md cursor-poiner hover:bg-neutral-100 dark:hover:text-black'
+                  href='/privacy'
+                >
+                  Privacy Policy
+                </NavigationMenu.Link>
 
-            <span> | </span>
+                <span> | </span>
 
-            <a
-              href='/terms'
-              className='p-2 px-2 text-sm transition ease-in-out rounded-md cursor-pointer hover:bg-neutral-100 dark:hover:text-black'
-            >
-              Terms of Use
-            </a>
-          </div>
+                <NavigationMenu.Link
+                  className='p-2 px-2 text-sm transition ease-in-out rounded-md cursor-poiner hover:bg-neutral-100 dark:hover:text-black'
+                  href='/terms'
+                >
+                  Terms of Use
+                </NavigationMenu.Link>
+              </NavigationMenu.Item>
+            </NavigationMenu.List>
+          </NavigationMenu.Root>
         </div>
       </footer>
     </>
