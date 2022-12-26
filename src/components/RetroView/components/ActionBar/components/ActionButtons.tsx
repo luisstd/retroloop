9700da746d55f46742282865e3fffb926e94c290
Luis Steidle <luis@steidle.me>
2022-12-26 23:05:06 +0100

    refactor: remove unnecessary react fragment

--- src/components/RetroView/components/ActionBar/components/ActionButtons.tsx
+++ src/components/RetroView/components/ActionBar/components/ActionButtons.tsx
@@ -52,36 +52,34 @@ function ActionButtons(props: ActionButtonsProps) {
   }
 
   return (
-    <>
-      <div className='flex'>
-        <Toast.Provider swipeDirection='right'>
-          <button type='submit' aria-label='Start Retro' onClick={handleCopy}>
-            <div className='flex flex-row items-center gap-2 p-5 text-lg transition ease-in-out border-2 border-black rounded-md w-fit dark:border-neutral-200 hover:scale-105 hover:cursor-pointer'>
-              Copy Retro link
-              <IconCopy />
-            </div>
-          </button>
-
-          <Toast.Root open={open} onOpenChange={setOpen}>
-            <Toast.Title className='flex items-center gap-2 text-md'>
-              <span>Copied retrospective link to clipboard!</span>
-              <IconCopy />
-            </Toast.Title>
-          </Toast.Root>
-
-          {open ? (
-            <Toast.Viewport className='fixed bottom-0 right-0 gap-2 p-5 m-5 text-black bg-white border-2 border-black rounded-md dark:text-white dark:bg-black w-fit dark:border-neutral-200' />
-          ) : null}
-        </Toast.Provider>
-
-        <button type='submit' aria-label='Start Retro' className='mx-2' onClick={handleNextPhase}>
+    <div className='flex'>
+      <Toast.Provider swipeDirection='right'>
+        <button type='submit' aria-label='Start Retro' onClick={handleCopy}>
           <div className='flex flex-row items-center gap-2 p-5 text-lg transition ease-in-out border-2 border-black rounded-md w-fit dark:border-neutral-200 hover:scale-105 hover:cursor-pointer'>
-            Next Phase
-            <IconArrowRight />
+            Copy Retro link
+            <IconCopy />
           </div>
         </button>
-      </div>
-    </>
+
+        <Toast.Root open={open} onOpenChange={setOpen}>
+          <Toast.Title className='flex items-center gap-2 text-md'>
+            <span>Copied retrospective link to clipboard!</span>
+            <IconCopy />
+          </Toast.Title>
+        </Toast.Root>
+
+        {open ? (
+          <Toast.Viewport className='fixed bottom-0 right-0 gap-2 p-5 m-5 text-black bg-white border-2 border-black rounded-md dark:text-white dark:bg-black w-fit dark:border-neutral-200' />
+        ) : null}
+      </Toast.Provider>
+
+      <button type='submit' aria-label='Start Retro' className='mx-2' onClick={handleNextPhase}>
+        <div className='flex flex-row items-center gap-2 p-5 text-lg transition ease-in-out border-2 border-black rounded-md w-fit dark:border-neutral-200 hover:scale-105 hover:cursor-pointer'>
+          Next Phase
+          <IconArrowRight />
+        </div>
+      </button>
+    </div>
   )
 }
 
