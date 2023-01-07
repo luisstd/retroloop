fb301de21dc6fd95a88dc761a8baa9227f755e82
Luis Steidle <luis@steidle.me>
2023-01-08 00:14:52 +0100

    chore: add custom base and hover colors

--- tailwind.config.js
+++ tailwind.config.js
@@ -3,7 +3,18 @@ module.exports = {
   darkMode: 'class',
   content: ['./src/pages/**/*.{js,ts,jsx,tsx}', './src/components/**/*.{js,ts,jsx,tsx}'],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        base: {
+          light: 'rgba(229,229,229,1)',
+          dark: 'rgba(18,18,18,1)',
+        },
+        hover: {
+          light: 'rgba(209, 213, 219,0.5)',
+          dark: 'rgba(229,229,229,1)',
+        },
+      },
+    },
+    plugins: [],
   },
-  plugins: [],
 }
