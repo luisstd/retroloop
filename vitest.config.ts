d62a3db66c1f75279989d892d21614eaee2643e1
Luis Steidle <luis@steidle.me>
2023-02-11 02:48:03 +0100

    test: configure vitest coverage files

--- vitest.config.ts
+++ vitest.config.ts
@@ -6,5 +6,9 @@ export default defineConfig({
   plugins: [react()],
   test: {
     environment: 'jsdom',
+    coverage: {
+      reporter: ['text', 'json', 'html'],
+      reportsDirectory: './tests/unit/coverage',
+    },
   },
 })
