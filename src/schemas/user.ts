57ebb2a0f07dd75499cadc1634ac57b4a492acda
Luis Steidle <luis@steidle.me>
2023-02-16 21:49:56 +0100

    fix: make user name nullable

--- src/schemas/user.ts
+++ src/schemas/user.ts
@@ -2,11 +2,11 @@ import { z } from 'zod'
 
 export const UserCreateInputSchema = z.object({
   email: z.string(),
-  name: z.string(),
+  name: z.string().nullable(),
 })
 
 export const UserUpdateInputSchema = z.object({
   id: z.string(),
   email: z.string(),
-  name: z.string(),
+  name: z.string().nullable(),
 })
