94b0ecbe52de8cc4b6b7ff7a4999ce51f84dc81b
Luis Steidle <luis@steidle.me>
2023-04-26 18:56:22 +0200

    chore: switch to react-swc vitest plugin

--- vitest.config.ts
+++ vitest.config.ts
@@ -1,10 +1,10 @@
-import react from '@vitejs/plugin-react'
+import react from '@vitejs/plugin-react-swc'
 import path from 'path'
 import { defineConfig } from 'vitest/config'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
+  plugins: [react()] as any,
   test: {
     environment: 'jsdom',
     coverage: {
