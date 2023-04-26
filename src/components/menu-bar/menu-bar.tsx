9c48928f93fd2ef96d95bdfa1537a830a2b110f9
Luis Steidle <luis@steidle.me>
2023-04-26 18:56:02 +0200

    chore: update to next 13.3.1

--- src/components/menu-bar/menu-bar.tsx
+++ src/components/menu-bar/menu-bar.tsx
@@ -14,7 +14,7 @@ export default function MenuBar() {
     <div className='flex items-center w-screen max-w-screen-2xl'>
       <NavigationMenu.Root className='relative flex items-center justify-between w-full p-2 m-5 text-xl border-2 rounded-md shadow-md border-base-dark dark:border-base-light max-w-screen-2xl bg-base-light dark:bg-base-dark'>
         <NavigationMenu.List className='flex items-center gap-2'>
-          <Link href='/'>
+          <Link href='/' legacyBehavior>
             <NavigationMenu.Item className='flex items-center text-2xl italic text-center sm:text-3xl'>
               <IconInfinity className='sm:hidden' size={28} />
               <IconInfinity className='hidden sm:block' size={40} />
@@ -26,14 +26,14 @@ export default function MenuBar() {
         </NavigationMenu.List>
 
         <NavigationMenu.List className='hidden gap-6 sm:flex'>
-          <Link href='/dashboard'>
+          <Link href='/dashboard' legacyBehavior>
             <NavigationMenu.Item className='flex items-center gap-1 p-2 transition ease-in-out rounded-md cursor-pointer hover:bg-hover-light dark:hover:bg-hover-dark dark:hover:text-base-dark'>
               <IconSquare size={24} />
               <NavigationMenu.Link>Dashboard</NavigationMenu.Link>
             </NavigationMenu.Item>
           </Link>
 
-          <Link href='/settings'>
+          <Link href='/settings' legacyBehavior>
             <NavigationMenu.Item className='flex items-center gap-1 p-2 transition ease-in-out rounded-md cursor-pointer hover:bg-hover-light dark:hover:bg-hover-dark dark:hover:text-base-dark'>
               <IconDotsCircleHorizontal size={24} />
               <NavigationMenu.Link>Settings</NavigationMenu.Link>
