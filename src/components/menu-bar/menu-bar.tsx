393431facbce6b4a733c986f6f9068601d315708
Luis Steidle <luis@steidle.me>
2023-06-03 14:51:39 +0200

    feat: redirect to login page on click when unauthenticated

--- src/components/menu-bar/menu-bar.tsx
+++ src/components/menu-bar/menu-bar.tsx
@@ -1,7 +1,7 @@
 import * as NavigationMenu from '@radix-ui/react-navigation-menu'
 import { IconDotsCircleHorizontal, IconInfinity, IconSquare } from '@tabler/icons-react'
 import Link from 'next/link'
-import { useSession } from 'next-auth/react'
+import { signIn, useSession } from 'next-auth/react'
 
 import { LoginButton } from '@/components/menu-bar/components/login-button'
 import { NavigationDropdown } from '@/components/menu-bar/components/nav-dropdown'
@@ -9,7 +9,10 @@ import { ThemeDropdown } from '@/components/menu-bar/components/theme-dropdown'
 import { UserDropdown } from '@/components/menu-bar/components/user-dropdown'
 
 export function MenuBar() {
-  const { data: session } = useSession()
+  const { data: session, status } = useSession()
+
+  const isSignedUp = status === 'authenticated' && session?.user?.name !== null
+
   return (
     <div className='flex items-center w-screen max-w-screen-2xl'>
       <NavigationMenu.Root className='relative flex items-center justify-between w-full p-2 m-5 text-xl border-2 rounded-md shadow-md border-base-dark dark:border-base-light max-w-screen-2xl bg-base-light dark:bg-base-dark'>
@@ -27,17 +30,37 @@ export function MenuBar() {
 
         <NavigationMenu.List className='hidden gap-6 sm:flex'>
           <Link href='/dashboard' legacyBehavior>
-            <NavigationMenu.Item className='flex items-center gap-1 p-2 transition ease-in-out rounded-md cursor-pointer hover:bg-hover-light dark:hover:bg-hover-dark dark:hover:text-base-dark'>
-              <IconSquare size={24} />
-              <NavigationMenu.Link>Dashboard</NavigationMenu.Link>
-            </NavigationMenu.Item>
+            {isSignedUp ? (
+              <NavigationMenu.Item className='flex items-center gap-1 p-2 transition ease-in-out rounded-md cursor-pointer hover:bg-hover-light dark:hover:bg-hover-dark dark:hover:text-base-dark'>
+                <IconSquare size={24} />
+                <NavigationMenu.Link>Dashboard</NavigationMenu.Link>
+              </NavigationMenu.Item>
+            ) : (
+              <NavigationMenu.Item
+                onClick={() => signIn()}
+                className='flex items-center gap-1 p-2 transition ease-in-out rounded-md cursor-pointer hover:bg-hover-light dark:hover:bg-hover-dark dark:hover:text-base-dark'
+              >
+                <IconSquare size={24} />
+                <NavigationMenu.Link>Dashboard</NavigationMenu.Link>
+              </NavigationMenu.Item>
+            )}
           </Link>
 
           <Link href='/settings' legacyBehavior>
-            <NavigationMenu.Item className='flex items-center gap-1 p-2 transition ease-in-out rounded-md cursor-pointer hover:bg-hover-light dark:hover:bg-hover-dark dark:hover:text-base-dark'>
-              <IconDotsCircleHorizontal size={24} />
-              <NavigationMenu.Link>Settings</NavigationMenu.Link>
-            </NavigationMenu.Item>
+            {isSignedUp ? (
+              <NavigationMenu.Item className='flex items-center gap-1 p-2 transition ease-in-out rounded-md cursor-pointer hover:bg-hover-light dark:hover:bg-hover-dark dark:hover:text-base-dark'>
+                <IconDotsCircleHorizontal size={24} />
+                <NavigationMenu.Link>Settings</NavigationMenu.Link>
+              </NavigationMenu.Item>
+            ) : (
+              <NavigationMenu.Item
+                onClick={() => signIn()}
+                className='flex items-center gap-1 p-2 transition ease-in-out rounded-md cursor-pointer hover:bg-hover-light dark:hover:bg-hover-dark dark:hover:text-base-dark'
+              >
+                <IconDotsCircleHorizontal size={24} />
+                <NavigationMenu.Link>Settings</NavigationMenu.Link>
+              </NavigationMenu.Item>
+            )}
           </Link>
         </NavigationMenu.List>
 
