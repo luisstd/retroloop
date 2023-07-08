31aacbe9d7ca4a3b98df6a0ce5d500601ed8337d
Luis Steidle <luis@steidle.me>
2023-07-09 01:06:39 +0200

    chore: rename auth & db server files

--- server/trpc/context.ts
+++ server/trpc/context.ts
@@ -3,8 +3,8 @@ import * as trpc from '@trpc/server'
 import * as trpcNext from '@trpc/server/adapters/next'
 import { Session } from 'next-auth'
 
-import { getServerAuthSession } from '../common/get-server-auth-session'
-import { prisma } from '../db/client'
+import { getServerAuthSession } from '../auth'
+import { prisma } from '../db'
 
 type CreateContextOptions = {
   session: Session | null
