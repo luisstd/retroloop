56dfb9bf49556d3c9549be70849d4d89126a984c
Luis Steidle <luis@steidle.me>
2023-07-09 16:15:37 +0200

    chore: move globals.css

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,4 +1,4 @@
-import '@/styles/globals.css'
+import '@/app/globals.css'
 
 import { QueryClient, QueryClientProvider } from '@tanstack/react-query'
 import { ReactQueryDevtools } from '@tanstack/react-query-devtools'
