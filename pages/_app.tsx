a617a3e4c57b76374231133cc432bb3d83dcbe4d
Luis Steidle <luis@steidle.me>
2023-07-11 20:41:02 +0200

    refactor: move global styles file

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,4 +1,4 @@
-import '@/globals.css'
+import '@/styles/globals.css'
 
 import { QueryClient, QueryClientProvider } from '@tanstack/react-query'
 import { ReactQueryDevtools } from '@tanstack/react-query-devtools'
