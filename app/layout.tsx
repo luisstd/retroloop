26ffb5daec200ba20e21292d4e1efe819928a536
Luis Steidle <luis@steidle.me>
2023-07-11 22:26:24 +0200

    refactor: add new root layout

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,7 +1,23 @@
-export default function RootLayout({ children }: { children: React.ReactNode }) {
+import { QueryClient } from '@tanstack/react-query'
+import { getServerSession } from 'next-auth'
+import { authOptions } from 'pages/api/auth/[...nextauth]'
+
+import { Providers } from '@/providers'
+import { space_grotesk } from '@/styles/fonts'
+
+export default async function RootLayout({ children }: { children: React.ReactNode }) {
+  const query_client = new QueryClient()
+  const { data: session } = await getServerSession(authOptions)
+
   return (
-    <html lang='en'>
-      <body>{children}</body>
-    </html>
+    session && (
+      <html lang='en'>
+        <body>
+          <Providers queryClient={query_client} session={session}>
+            <main className={space_grotesk.className}>{children}</main>
+          </Providers>
+        </body>
+      </html>
+    )
   )
 }
