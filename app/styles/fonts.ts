dec83025e95db96a9c54b6515f468331ce69fb7c
Luis Steidle <luis@steidle.me>
2023-07-11 22:18:56 +0200

    refactor: move font loader to separate file

---
+++ app/styles/fonts.ts
@@ -0,0 +1,7 @@
+import { Space_Grotesk } from 'next/font/google'
+
+export const space_grotesk = Space_Grotesk({
+  weight: ['300', '400', '500', '600', '700'],
+  style: ['normal'],
+  subsets: ['latin'],
+})
