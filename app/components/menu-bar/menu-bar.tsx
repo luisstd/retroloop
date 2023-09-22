745fd185d96b754c7c7e55619b517cf766d15be3
Luis Steidle <luis@steidle.me>
2023-09-22 20:08:18 +0200

    refactor: refactor nav menu

--- app/components/menu-bar/menu-bar.tsx
+++ app/components/menu-bar/menu-bar.tsx
@@ -1,7 +1,6 @@
 'use client'
 
-import * as NavigationMenu from '@radix-ui/react-navigation-menu'
-import { IconDotsCircleHorizontal, IconInfinity, IconSquare } from '@tabler/icons-react'
+import { IconDotsCircleHorizontal, IconInfinity, IconLayoutGrid } from '@tabler/icons-react'
 import Link from 'next/link'
 import { signIn, useSession } from 'next-auth/react'
 
@@ -9,6 +8,14 @@ import { LoginButton } from '@/components/menu-bar/components/login-button/login
 import { NavigationDropdown } from '@/components/menu-bar/components/nav-dropdown/nav-dropdown'
 import { ThemeDropdown } from '@/components/menu-bar/components/theme-dropdown/theme-dropdown'
 import { UserDropdown } from '@/components/menu-bar/components/user-dropdown/user-dropdown'
+import { Button } from '@/ui/button/button'
+import {
+  NavigationMenu,
+  NavigationMenuItem,
+  NavigationMenuLink,
+  NavigationMenuList,
+  NavigationMenuViewport,
+} from '@/ui/navigation-menu/navigation-menu'
 
 export function MenuBar() {
   const { data: session, status } = useSession()
@@ -17,65 +24,61 @@ export function MenuBar() {
 
   return (
     <div className='flex w-screen max-w-screen-2xl items-center'>
-      <NavigationMenu.Root className='relative m-5 flex w-full max-w-screen-2xl items-center justify-between rounded-md border-2 border-base-dark bg-base-light p-2 text-xl shadow-md dark:border-base-light dark:bg-base-dark'>
-        <NavigationMenu.List className='flex items-center gap-2'>
+      <NavigationMenu className='border-base-dark bg-base-light dark:border-base-light dark:bg-base-dark relative m-5 flex w-full max-w-screen-2xl items-center justify-between rounded-md border-2 p-2 text-xl shadow-md'>
+        <NavigationMenuList className='flex items-center gap-2'>
           <Link href='/'>
-            <NavigationMenu.Item className='text-center text-2xl italic sm:text-3xl'>
-              <div className='items-top flex cursor-pointer rounded-md p-1 pr-2 font-semibold transition ease-in-out hover:bg-hover-light dark:hover:bg-hover-dark dark:hover:text-base-dark'>
+            <NavigationMenuLink>
+              <NavigationMenuItem className='items-top flex cursor-pointer text-center text-2xl font-bold italic sm:text-3xl'>
                 <IconInfinity className='self-center sm:hidden' size={28} />
                 <IconInfinity className='hidden sm:block' size={40} />
                 retroloop
-              </div>
-            </NavigationMenu.Item>
+              </NavigationMenuItem>
+            </NavigationMenuLink>
           </Link>
-        </NavigationMenu.List>
+        </NavigationMenuList>
 
-        <NavigationMenu.List className='hidden gap-6 sm:flex'>
+        <NavigationMenuList className='hidden gap-6 sm:flex'>
           <Link href='/dashboard'>
-            {isSignedUp ? (
-              <NavigationMenu.Item className='flex cursor-pointer items-center gap-1 rounded-md p-2 transition ease-in-out hover:bg-hover-light dark:hover:bg-hover-dark dark:hover:text-base-dark'>
-                <IconSquare size={24} />
-                <div className='text-xl font-medium'>Dashboard</div>
-              </NavigationMenu.Item>
-            ) : (
-              <NavigationMenu.Item
-                onClick={() => signIn()}
-                className='flex cursor-pointer items-center gap-1 rounded-md p-2 transition ease-in-out hover:bg-hover-light dark:hover:bg-hover-dark dark:hover:text-base-dark'
-              >
-                <IconSquare size={24} />
-                <div className='text-xl font-medium'>Dashboard</div>
-              </NavigationMenu.Item>
-            )}
+            <NavigationMenuLink>
+              <NavigationMenuItem>
+                <Button
+                  className='flex gap-2 text-lg'
+                  variant='ghost'
+                  onClick={!isSignedUp ? () => signIn() : undefined}
+                >
+                  <IconLayoutGrid size={20} />
+                  Dashboard
+                </Button>
+              </NavigationMenuItem>
+            </NavigationMenuLink>
           </Link>
 
           <Link href='/settings'>
-            {isSignedUp ? (
-              <NavigationMenu.Item className='flex cursor-pointer items-center gap-1 rounded-md p-2 transition ease-in-out hover:bg-hover-light dark:hover:bg-hover-dark dark:hover:text-base-dark'>
-                <IconDotsCircleHorizontal size={24} />
-                <div className='text-xl font-medium'>Settings</div>
-              </NavigationMenu.Item>
-            ) : (
-              <NavigationMenu.Item
-                onClick={() => signIn()}
-                className='flex cursor-pointer items-center gap-1 rounded-md p-2 transition ease-in-out hover:bg-hover-light dark:hover:bg-hover-dark dark:hover:text-base-dark'
-              >
-                <IconDotsCircleHorizontal size={24} />
-                <div className='text-xl font-medium'>Settings</div>
-              </NavigationMenu.Item>
-            )}
+            <NavigationMenuLink>
+              <NavigationMenuItem className='hover:bg-hover-light dark:hover:bg-hover-dark dark:hover:text-base-dark flex cursor-pointer items-center gap-1 rounded-md p-2 transition ease-in-out'>
+                <Button
+                  className='flex gap-2 text-lg'
+                  variant='ghost'
+                  onClick={!isSignedUp ? () => signIn() : undefined}
+                >
+                  <IconDotsCircleHorizontal size={20} />
+                  Settings
+                </Button>
+              </NavigationMenuItem>
+            </NavigationMenuLink>
           </Link>
-        </NavigationMenu.List>
+        </NavigationMenuList>
 
         <div className='flex items-center gap-2'>
-          <NavigationMenu.List className='flex gap-2 sm:hidden'>
+          <NavigationMenuList className='flex gap-2 sm:hidden'>
             <NavigationDropdown />
-          </NavigationMenu.List>
+          </NavigationMenuList>
           <ThemeDropdown />
           {session?.user ? <UserDropdown /> : <LoginButton />}
         </div>
 
-        <NavigationMenu.Viewport />
-      </NavigationMenu.Root>
+        <NavigationMenuViewport />
+      </NavigationMenu>
     </div>
   )
 }
