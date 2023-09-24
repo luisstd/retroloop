dbdc55dafdf668adcdf9219970470c18841905cf
Luis Steidle <luis@steidle.me>
2023-09-24 15:06:03 +0200

    refactor: refactor menubar

--- app/components/menu-bar/menu-bar.tsx
+++ app/components/menu-bar/menu-bar.tsx
@@ -1,6 +1,6 @@
 'use client'
 
-import { IconDotsCircleHorizontal, IconInfinity, IconLayoutGrid } from '@tabler/icons-react'
+import { IconInfinity } from '@tabler/icons-react'
 import Link from 'next/link'
 import { signIn, useSession } from 'next-auth/react'
 
@@ -24,61 +24,51 @@ export function MenuBar() {
   const isSignedUp = status === 'authenticated' && session?.user?.name !== null
 
   return (
-    <div className='flex w-screen max-w-screen-2xl items-center'>
-      <NavigationMenu className='border-base-dark bg-base-light dark:border-base-light dark:bg-base-dark relative m-5 flex w-full max-w-screen-2xl items-center justify-between rounded-md border-2 p-2 text-xl shadow-md'>
-        <NavigationMenuList className='flex items-center gap-2'>
-          <NavigationMenuItem className='items-top flex cursor-pointer text-center text-2xl font-bold italic sm:text-3xl '>
-            <Link href='/' legacyBehavior>
-              <NavigationMenuLink
-                className={cn(
-                  navigationMenuTriggerStyle(),
-                  'items-top flex cursor-pointer text-center text-2xl font-bold italic sm:text-3xl'
-                )}
-              >
-                <IconInfinity className='self-center sm:hidden' size={28} />
-                <IconInfinity className='hidden sm:block' size={40} />
-                retroloop
-              </NavigationMenuLink>
-            </Link>
-          </NavigationMenuItem>
-        </NavigationMenuList>
+    <NavigationMenu className='m-5 flex items-center gap-2 rounded-full border-2 p-1 text-xl shadow-sm'>
+      <NavigationMenuItem className='items-top flex cursor-pointer text-center text-2xl font-bold italic sm:text-3xl '>
+        <Link href='/' legacyBehavior passHref>
+          <NavigationMenuLink
+            className={cn(
+              navigationMenuTriggerStyle(),
+              'cursor-pointer rounded-l-3xl rounded-r-md'
+            )}
+          >
+            <IconInfinity className='self-center' size={28} />
+          </NavigationMenuLink>
+        </Link>
+      </NavigationMenuItem>
 
-        <NavigationMenuList className='hidden gap-6 sm:flex'>
-          <NavigationMenuItem>
-            <Link href='/dashboard' legacyBehavior>
-              <NavigationMenuLink
-                className={cn(navigationMenuTriggerStyle(), 'flex cursor-pointer gap-2 text-lg')}
-                onClick={!isSignedUp ? () => signIn() : undefined}
-              >
-                <IconLayoutGrid size={20} />
-                Dashboard
-              </NavigationMenuLink>
-            </Link>
-          </NavigationMenuItem>
+      <NavigationMenuList className='hidden sm:flex'>
+        <NavigationMenuItem>
+          <Link href='/dashboard' legacyBehavior passHref>
+            <NavigationMenuLink
+              className={cn(navigationMenuTriggerStyle(), 'cursor-pointer')}
+              onClick={!isSignedUp ? () => signIn() : undefined}
+            >
+              Dashboard
+            </NavigationMenuLink>
+          </Link>
+        </NavigationMenuItem>
 
-          <NavigationMenuItem>
-            <Link href='/settings' legacyBehavior>
-              <NavigationMenuLink
-                className={cn(navigationMenuTriggerStyle(), 'flex cursor-pointer gap-2 text-lg')}
-                onClick={!isSignedUp ? () => signIn() : undefined}
-              >
-                <IconDotsCircleHorizontal size={20} />
-                Settings
-              </NavigationMenuLink>
-            </Link>
-          </NavigationMenuItem>
-        </NavigationMenuList>
+        <NavigationMenuItem>
+          <Link href='/settings' legacyBehavior passHref>
+            <NavigationMenuLink
+              className={cn(navigationMenuTriggerStyle(), 'cursor-pointer')}
+              onClick={!isSignedUp ? () => signIn() : undefined}
+            >
+              Profile
+            </NavigationMenuLink>
+          </Link>
+        </NavigationMenuItem>
+      </NavigationMenuList>
 
-        <div className='flex items-center gap-2'>
-          <NavigationMenuList className='flex gap-2 sm:hidden'>
-            <NavigationDropdown />
-          </NavigationMenuList>
-          <ThemeDropdown />
-          {session?.user ? <UserDropdown /> : <LoginButton />}
-        </div>
+      <div>
+        <NavigationDropdown />
+        <ThemeDropdown />
+        {session?.user ? <UserDropdown /> : <LoginButton />}
+      </div>
 
-        <NavigationMenuViewport />
-      </NavigationMenu>
-    </div>
+      <NavigationMenuViewport />
+    </NavigationMenu>
   )
 }
