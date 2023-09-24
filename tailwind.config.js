0cd5265fe9d9cb125f660197672e8f3ada479d1b
Luis Steidle <luis@steidle.me>
2023-09-24 15:33:31 +0200

    chore: enable tailwind prose plugin

--- tailwind.config.js
+++ tailwind.config.js
@@ -71,4 +71,4 @@ export const theme = {
     },
   },
 }
-export const plugins = [require('tailwindcss-animate')]
+export const plugins = [require('tailwindcss-animate'), require('@tailwindcss/typography')]
