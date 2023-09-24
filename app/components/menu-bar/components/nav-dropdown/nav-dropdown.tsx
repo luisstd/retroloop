3f72ac140cd10ea693257c4968fa141fcc17cf6b
Luis Steidle <luis@steidle.me>
2023-09-24 15:05:47 +0200

    refactor: refactor nav dropdown

--- app/components/menu-bar/components/nav-dropdown/nav-dropdown.tsx
+++ app/components/menu-bar/components/nav-dropdown/nav-dropdown.tsx
@@ -1,48 +1,50 @@
 'use client'
 
-import * as DropdownMenu from '@radix-ui/react-dropdown-menu'
-import * as NavigationMenu from '@radix-ui/react-navigation-menu'
-import { IconDotsCircleHorizontal, IconMenu, IconSquare } from '@tabler/icons-react'
+import { IconMenu } from '@tabler/icons-react'
 import Link from 'next/link'
 import { useRouter } from 'next/router'
+
+import { Button } from '@/ui/button/button'
+import {
+  DropdownMenu,
+  DropdownMenuContent,
+  DropdownMenuItem,
+  DropdownMenuPortal,
+  DropdownMenuTrigger,
+} from '@/ui/dropdown-menu/dropdown-menu'
+import { NavigationMenuLink } from '@/ui/navigation-menu/navigation-menu'
 
 export function NavigationDropdown() {
   const router = useRouter()
   const currentPage = router.pathname
 
   return (
-    <>
-      <DropdownMenu.Root>
-        <DropdownMenu.Trigger>
-          <IconMenu size={30} />
-        </DropdownMenu.Trigger>
+    <DropdownMenu>
+      <DropdownMenuTrigger asChild className='sm:hidden'>
+        <Button variant='ghost'>
+          <IconMenu size={24} />
+        </Button>
+      </DropdownMenuTrigger>
 
-        <DropdownMenu.Portal>
-          <DropdownMenu.Content className='m-2 w-full gap-1 rounded-md border-2 border-base-dark bg-base-light pt-1 dark:border-base-light dark:bg-base-dark dark:text-base-light'>
-            <DropdownMenu.Label />
-            <DropdownMenu.Item />
-            <Link href='/dashboard'>
-              <NavigationMenu.Item
-                className={`mx-0 flex w-full items-center gap-1 px-1 py-1 hover:bg-neutral-100 dark:hover:text-base-dark
-              ${currentPage === '/dashboard' ? 'font-bold' : 'font-normal'}`}
-              >
-                <IconSquare size={26} />
-                <NavigationMenu.Link>Dashboard</NavigationMenu.Link>
-              </NavigationMenu.Item>
+      <DropdownMenuPortal>
+        <DropdownMenuContent>
+          <DropdownMenuItem
+            className={`flex ${currentPage === '/dashboard' ? 'font-bold' : 'font-normal'}`}
+          >
+            <Link href='/dashboard' legacyBehavior passHref>
+              <NavigationMenuLink>Dashboard</NavigationMenuLink>
             </Link>
+          </DropdownMenuItem>
 
-            <Link href='/settings'>
-              <NavigationMenu.Item
-                className={`mx-0 flex w-full items-center gap-1 px-1 py-1 hover:bg-neutral-100 dark:hover:text-base-dark
-              ${currentPage === '/settings' ? 'font-bold' : 'font-normal'}`}
-              >
-                <IconDotsCircleHorizontal size={26} />
-                <NavigationMenu.Link>Settings</NavigationMenu.Link>
-              </NavigationMenu.Item>
+          <DropdownMenuItem
+            className={`flex ${currentPage === '/settings' ? 'font-bold' : 'font-normal'}`}
+          >
+            <Link href='/settings' legacyBehavior passHref>
+              <NavigationMenuLink>Settings</NavigationMenuLink>
             </Link>
-          </DropdownMenu.Content>
-        </DropdownMenu.Portal>
-      </DropdownMenu.Root>
-    </>
+          </DropdownMenuItem>
+        </DropdownMenuContent>
+      </DropdownMenuPortal>
+    </DropdownMenu>
   )
 }
