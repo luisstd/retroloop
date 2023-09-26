d5fb3035e1d05e88d7ee60c22987fd1a75c15083
Luis Steidle <luis@steidle.me>
2023-09-26 23:11:24 +0200

    refactor: refactor profile section

--- app/components/profile-section/profile-section.tsx
+++ app/components/profile-section/profile-section.tsx
@@ -1,25 +1,28 @@
 'use client'
 
 import { User } from '@prisma/client'
-import * as Avatar from '@radix-ui/react-avatar'
-import * as Label from '@radix-ui/react-label'
-import * as Toast from '@radix-ui/react-toast'
-import { IconCheck, IconUserCircle } from '@tabler/icons-react'
+import { IconUserCircle } from '@tabler/icons-react'
 import { Field, Form, Formik } from 'formik'
 import { useRouter } from 'next/router'
-import { useState } from 'react'
+import { version } from 'package.json'
 import { toFormikValidationSchema } from 'zod-formik-adapter'
 
 import { DeleteUserDialog } from '@/components/profile-section/components/delete-user-dialog'
 import { UserUpdateInputSchema } from '@/schemas/user'
 import { UserUpdateInput } from '@/types/user'
+import { Avatar, AvatarFallback, AvatarImage } from '@/ui/avatar/avatar'
+import { Badge } from '@/ui/badge/badge'
+import { Button } from '@/ui/button/button'
+import { Card, CardHeader, CardTitle } from '@/ui/card/card'
+import { Input } from '@/ui/input/input'
+import { Label } from '@/ui/label/label'
+import { useToast } from '@/ui/toast/use-toast'
 import { trpc } from '@/utils/trpc'
 
 export function ProfileSection() {
   const user = trpc.user.getLoggedIn.useQuery()
   const router = useRouter()
-
-  const [open, setOpen] = useState(false)
+  const { toast } = useToast()
 
   const mutationEdit = trpc.user.edit.useMutation({
     onSuccess: () => {
@@ -35,104 +38,81 @@ export function ProfileSection() {
 
   function handleSubmit(input: UserUpdateInput): void {
     mutationEdit.mutate(input)
-    setOpen(true)
+    toast({
+      title: 'Profile updated',
+      description: 'Your profile was successfully updated',
+    })
   }
 
   function handleDelete(input: User): void {
     mutationDelete.mutate(input)
-    setOpen(true)
+    toast({
+      title: 'Account deleted',
+      description: 'Your account was successfully deleted',
+    })
   }
 
   return (
-    <>
-      <Toast.Provider swipeDirection='right'>
-        <Toast.Root open={open} onOpenChange={setOpen}>
-          <Toast.Title className='text-md flex items-center gap-2'>
-            <span>Success</span>
-            <IconCheck />
-          </Toast.Title>
-        </Toast.Root>
+    <Card className='w-[calc(100%-2.5rem)] bg-background p-10 shadow-sm'>
+      <CardHeader className='flex flex-row items-baseline justify-between'>
+        <CardTitle>PROFILE</CardTitle>
+        <Badge>Version {version}</Badge>
+      </CardHeader>
 
-        {open ? (
-          <Toast.Viewport className='border-base-dark bg-base-light text-base-dark dark:border-base-light dark:bg-base-dark dark:text-base-light fixed bottom-0 right-0 m-5 w-fit gap-2 rounded-md border-2 p-5 ' />
-        ) : null}
+      <Formik
+        validationSchema={toFormikValidationSchema(UserUpdateInputSchema)}
+        initialValues={{
+          id: user.data?.id || '',
+          name: user.data?.name || '',
+          email: user.data?.email || '',
+          image: user.data?.image || '',
+          role: user.data?.role || '',
+          createdAt: user.data?.createdAt,
+          emailVerified: user.data?.emailVerified,
+        }}
+        onSubmit={(values) => handleSubmit(values)}
+      >
+        <Form className='mx-auto w-10/12'>
+          <div className='flex items-end justify-around'>
+            <section className='flex flex-col gap-5'>
+              <fieldset className='flex flex-col items-start gap-2'>
+                <Label htmlFor='name'>Name</Label>
+                <Field as={Input} id='name' name='name' type='text' placeholder='Your Name' />
+              </fieldset>
 
-        {user.data ? (
-          <main className='flex w-full max-w-screen-2xl justify-around gap-5'>
-            <section className='border-base-dark dark:border-base-light mx-5 min-w-min flex-grow rounded-md border-2 p-5 text-xl shadow-md'>
-              <Formik
-                validationSchema={toFormikValidationSchema(UserUpdateInputSchema)}
-                initialValues={{
-                  id: user.data.id || '',
-                  name: user.data.name || '',
-                  email: user.data.email || '',
-                  image: user.data.image || '',
-                  role: user.data.role || '',
-                  createdAt: user.data.createdAt,
-                  emailVerified: user.data.emailVerified,
-                }}
-                onSubmit={(values) => handleSubmit(values)}
-              >
-                <Form className='flex w-full flex-col items-center md:flex-row md:justify-around'>
-                  <div className='flex flex-col place-items-center items-start gap-5'>
-                    <fieldset className='flex flex-col items-start'>
-                      <Label.Root htmlFor='name' className='px-2 font-bold'>
-                        Name:
-                      </Label.Root>
-                      <Field
-                        id='name'
-                        name='name'
-                        type='text'
-                        placeholder='Your Name'
-                        className='border-base-dark dark:border-base-light m-2 rounded-md border-2 bg-neutral-200 p-2 outline-2 dark:bg-neutral-700'
-                      />
-                    </fieldset>
+              <fieldset className='flex flex-col items-start gap-2'>
+                <Label htmlFor='email'>E-Mail</Label>
+                <Field
+                  as={Input}
+                  id='email'
+                  name='email'
+                  type='email'
+                  placeholder='user@mail.com'
+                />
+              </fieldset>
 
-                    <fieldset className='flex flex-col items-start'>
-                      <Label.Root htmlFor='email' className='px-2 font-bold'>
-                        E-Mail:
-                      </Label.Root>
-                      <Field
-                        id='email'
-                        name='email'
-                        type='email'
-                        placeholder='user@mail.com'
-                        className='border-base-dark dark:border-base-light m-2 rounded-md border-2 bg-neutral-200 p-2 outline-2 dark:bg-neutral-700'
-                      />
-                    </fieldset>
-
-                    <button className='btn m-2 mt-10 px-2 py-1 font-bold italic' type='submit'>
-                      Submit
-                    </button>
-                  </div>
-
-                  <Avatar.Root>
-                    <Avatar.Image
-                      src={user.data?.image || ''}
-                      alt='User Avatar'
-                      className='border-base-dark dark:border-base-light hidden rounded border-2 hover:cursor-pointer sm:block'
-                      width={250}
-                      height='auto'
-                    />
-                    <Avatar.Fallback delayMs={300} className='hidden sm:block'>
-                      <IconUserCircle width={250} size={180} />
-                    </Avatar.Fallback>
-
-                    <div className='mt-5 flex flex-col items-center font-mono text-sm '>
-                      <b>Signed up at: </b>
-
-                      <p>{user.data.createdAt.toLocaleString()}</p>
-
-                      <DeleteUserDialog itemToDelete={user.data} deleteHandler={handleDelete} />
-                    </div>
-                  </Avatar.Root>
-                </Form>
-              </Formik>
-              <div className='flex flex-row items-center justify-around '></div>
+              <Button type='submit'>Submit</Button>
             </section>
-          </main>
-        ) : null}
-      </Toast.Provider>
-    </>
+
+            <section className='flex flex-col items-center gap-2'>
+              <Avatar className='h-32 w-32'>
+                <AvatarImage src={user.data?.image || ''} alt='User Avatar' />
+                <AvatarFallback delayMs={300} className='h-32 w-32'>
+                  <IconUserCircle size={64} />
+                </AvatarFallback>
+              </Avatar>
+              <div className='flex flex-col items-center gap-2'>
+                <Label>User since</Label>
+                <Badge variant='outline'>{user.data?.createdAt.toLocaleString()}</Badge>
+
+                {user.data ? (
+                  <DeleteUserDialog itemToDelete={user.data} deleteHandler={handleDelete} />
+                ) : null}
+              </div>
+            </section>
+          </div>
+        </Form>
+      </Formik>
+    </Card>
   )
 }
