9c020dd3d3647fecc75da46ad4f91bd7c659079f
Luis Steidle <luis@steidle.me>
2023-09-29 19:18:12 +0200

    refactor: refactor writing and voting view

--- app/components/retro-view/views/writing-phase/writing-view.tsx
+++ app/components/retro-view/views/writing-phase/writing-view.tsx
@@ -4,6 +4,7 @@ import { Retrospective } from '@prisma/client'
 
 import { RetroActionBar } from '@/components/retro-view/components/retro-action-bar'
 import { ItemCollector } from '@/components/retro-view/views/writing-phase/components/item-collector/item-collector'
+import { Card } from '@/ui/card/card'
 
 type WritingViewProps = {
   selectedRetro: Retrospective
@@ -11,36 +12,38 @@ type WritingViewProps = {
 
 export function WritingView({ selectedRetro }: WritingViewProps) {
   return (
-    <section className='mx-5 min-h-min w-full rounded-md border-2 border-black px-5 pb-2 dark:border-neutral-200 lg:h-full'>
+    <Card className='m-5 w-full p-5'>
       <div className='grid-rows-auto flex h-5/6 w-full grid-cols-3 flex-col place-items-center gap-5 lg:grid'>
         <RetroActionBar selectedRetro={selectedRetro} />
 
         {selectedRetro ? (
           <>
-            <div className='col-start-1 row-span-6 row-start-2 h-full w-full rounded-md border-2 border-black p-5 dark:border-neutral-200 lg:min-h-screen'>
+            <Card className='col-start-1 row-span-6 row-start-2 h-full w-full lg:min-h-screen'>
               <ItemCollector
                 retrospective={selectedRetro}
                 itemType='success'
                 title={'things that went well'}
               />
-            </div>
-            <div className='col-start-2 row-span-6 row-start-2 h-full w-full rounded-md border-2 border-black p-5 dark:border-neutral-200'>
+            </Card>
+
+            <Card className='col-start-2 row-span-6 row-start-2 h-full w-full lg:min-h-screen'>
               <ItemCollector
                 retrospective={selectedRetro}
                 itemType='improvement'
                 title={'things that can be improved'}
               />
-            </div>
-            <div className='col-start-3 row-span-6 row-start-2 h-full w-full rounded-md border-2 border-black p-5 dark:border-neutral-200'>
+            </Card>
+
+            <Card className='col-start-3 row-span-6 row-start-2 h-full w-full lg:min-h-screen'>
               <ItemCollector
                 retrospective={selectedRetro}
                 itemType='action'
                 title={'things to start doing'}
               />
-            </div>
+            </Card>
           </>
         ) : null}
       </div>
-    </section>
+    </Card>
   )
 }
