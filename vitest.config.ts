7ebc30ab86edd94d20eb60b13b6e881d415e2d8e
Luis Steidle <luis@steidle.me>
2023-09-30 13:14:48 +0200

    chore: fix vitest config

--- vitest.config.ts
+++ vitest.config.ts
@@ -1,10 +1,10 @@
-import react from '@vitejs/plugin-react-swc'
+import react from '@vitejs/plugin-react'
 import path from 'path'
 import { defineConfig } from 'vitest/config'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()] as any,
+  plugins: [react()],
   test: {
     environment: 'jsdom',
     coverage: {
