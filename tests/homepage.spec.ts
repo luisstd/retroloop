95aa95f94df7082a41bba0fe79a2472b45364ae6
Luis Steidle <luis@steidle.me>
2023-10-03 00:33:24 +0200

    test: add homepage integration test

---
+++ tests/homepage.spec.ts
@@ -0,0 +1,30 @@
+import { test } from '@playwright/test'
+
+test.describe('Landing Page Health Check', () => {
+  test('should navigate to Privacy Policy and Terms of Use pages', async ({ page }) => {
+    await page.goto('https://retroloop.io/')
+
+    // Navigate to Privacy Policy and verify
+    await page.getByRole('link', { name: 'Privacy Policy' }).click()
+    await page.waitForSelector('text=Retroloop Privacy Policy')
+
+    // Go back to the homepage and navigate to Terms of Use
+    await page.goBack()
+    await page.getByRole('link', { name: 'Terms of Use' }).click()
+    await page.waitForSelector('text=Retroloop Terms of Service')
+  })
+
+  test('should verify main navigation links and buttons', async ({ page }) => {
+    await page.goto('https://retroloop.io/')
+
+    // Verify main navigation links
+    page.locator('nav').filter({ hasText: 'Dashboard' }).getByRole('link').first()
+    page.locator('nav').filter({ hasText: 'Profile' }).getByRole('link').first()
+    page.locator('nav').filter({ hasText: 'Login' }).getByRole('link').first()
+
+    // Verify page buttons
+    page.locator('button').filter({ hasText: 'Get Started' }).first()
+    page.locator('button').filter({ hasText: 'Login' }).first()
+    page.locator('button').filter({ hasText: 'Github' }).first()
+  })
+})
