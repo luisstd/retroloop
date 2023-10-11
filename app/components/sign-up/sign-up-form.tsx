32bb0a7e9868e451d1d77a13551126c4ff06ed09
Luis Steidle <luis@steidle.me>
2023-10-11 20:43:44 +0200

    refactor: update sign-up form

--- app/components/sign-up/sign-up-form.tsx
+++ app/components/sign-up/sign-up-form.tsx
@@ -1,12 +1,23 @@
 'use client'
 
-import * as Avatar from '@radix-ui/react-avatar'
 import { IconUserCircle } from '@tabler/icons-react'
 import { Field, Form, Formik } from 'formik'
 import { useRouter } from 'next/router'
 import { useSession } from 'next-auth/react'
 
 import { UserSession } from '@/types/user'
+import { Avatar, AvatarFallback, AvatarImage } from '@/ui/avatar/avatar'
+import { Button } from '@/ui/button/button'
+import {
+  Card,
+  CardContent,
+  CardDescription,
+  CardFooter,
+  CardHeader,
+  CardTitle,
+} from '@/ui/card/card'
+import { Input } from '@/ui/input/input'
+import { Label } from '@/ui/label/label'
 import { trpc } from '@/utils/trpc'
 
 export function SignUpForm() {
@@ -26,8 +37,7 @@ export function SignUpForm() {
   return (
     <>
       {data ? (
-        <div className='flex w-screen max-w-screen-2xl flex-col items-center'>
-          <h1 className='my-5 text-3xl font-bold italic'>Complete login</h1>
+        <section className='mx-auto grid h-screen place-items-center'>
           <Formik
             initialValues={{
               id: data.user?.id || '',
@@ -39,53 +49,45 @@ export function SignUpForm() {
               handleSubmit(values)
             }}
           >
-            <Form className='border-base-dark bg-base-light dark:border-base-light dark:bg-base-dark dark:text-base-light flex w-screen max-w-md flex-col items-center gap-4 rounded-lg border-2 p-5'>
-              <Avatar.Root className='hover:cursor-pointer'>
-                <Avatar.Image src={data.user?.image || ''} alt='User Avatar' />
-                <Avatar.Fallback delayMs={600}>
-                  <IconUserCircle size={72} />
-                </Avatar.Fallback>
-              </Avatar.Root>
-              <fieldset className='flex w-full flex-col gap-4'>
-                <label htmlFor='name' className='font-bold'>
-                  Name
-                </label>
-                <Field
-                  id='name'
-                  name='name'
-                  component='input'
-                  rows={10}
-                  required
-                  className='border-base-dark dark:border-base-light h-full min-h-min w-full rounded-md border-2 bg-neutral-100 p-2 dark:bg-neutral-700'
-                />
+            <Card className='mx-5 sm:mx-0'>
+              <Form>
+                <CardHeader>
+                  <CardTitle>Complete login</CardTitle>
+                  <CardDescription>
+                    Please fill out the missing information for your account
+                  </CardDescription>
+                </CardHeader>
 
-                <label htmlFor='name' className='font-bold'>
-                  E-Mail
-                </label>
-                <Field
-                  id='email'
-                  name='email'
-                  component='input'
-                  rows={10}
-                  required
-                  disabled={data.user?.email}
-                  className={`h-full w-full border-2 p-2 ${
-                    data.user?.email ? 'italic text-gray-400 ' : ''
-                  }  border-base-dark dark:border-base-light min-h-min rounded-md bg-neutral-100 dark:bg-neutral-700`}
-                />
-              </fieldset>
-              <div className='flex w-full flex-row justify-end'>
-                <button
-                  type='submit'
-                  aria-label='Save'
-                  className='btn m-2 mt-10 px-2 py-1 font-bold italic'
-                >
-                  Continue
-                </button>
-              </div>
-            </Form>
+                <CardContent>
+                  <fieldset className='flex flex-col gap-4'>
+                    <Label htmlFor='name' className='font-bold'>
+                      Name
+                    </Label>
+                    <Field as={Input} id='name' name='name' required />
+
+                    <Label htmlFor='name' className='font-bold'>
+                      E-Mail
+                    </Label>
+                    <Field
+                      as={Input}
+                      id='email'
+                      name='email'
+                      required
+                      disabled={data.user?.email}
+                      className={`${data.user?.email ? 'italic text-muted-foreground ' : ''}`}
+                    />
+                  </fieldset>
+                </CardContent>
+
+                <CardFooter>
+                  <Button type='submit' aria-label='Save'>
+                    Continue
+                  </Button>
+                </CardFooter>
+              </Form>
+            </Card>
           </Formik>
-        </div>
+        </section>
       ) : null}
     </>
   )
