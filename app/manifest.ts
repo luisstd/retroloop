9425f4dc82191053788bb37fd5912f5280dce222
Luis Steidle <luis@steidle.me>
2023-10-15 13:00:16 +0200

    Merge branch 'dev' into 'main'

---
+++ app/manifest.ts
@@ -0,0 +1,20 @@
+import { MetadataRoute } from 'next'
+
+export default function manifest(): MetadataRoute.Manifest {
+  return {
+    name: 'Retroloop - Agile retrospectives made easy',
+    short_name: 'Retroloop',
+    description: 'Retroloop is a simple, open-source tool for facilitating agile retrospectives',
+    start_url: '/',
+    display: 'standalone',
+    background_color: '#ffffff',
+    theme_color: '#18181b',
+    icons: [
+      {
+        src: '/favicon.ico',
+        sizes: 'any',
+        type: 'image/x-icon',
+      },
+    ],
+  }
+}
---
+++ app/sitemap.ts
@@ -0,0 +1,36 @@
+import { MetadataRoute } from 'next'
+
+export default function sitemap(): MetadataRoute.Sitemap {
+  return [
+    {
+      url: 'https://retroloop.io',
+      lastModified: new Date(),
+      changeFrequency: 'yearly',
+      priority: 1,
+    },
+    {
+      url: 'https://retroloop.io/privacy',
+      lastModified: new Date(),
+      changeFrequency: 'monthly',
+      priority: 0.8,
+    },
+    {
+      url: 'https://retroloop.io/terms',
+      lastModified: new Date(),
+      changeFrequency: 'monthly',
+      priority: 0.8,
+    },
+    {
+      url: 'https://retroloop.io/dashboard',
+      lastModified: new Date(),
+      changeFrequency: 'monthly',
+      priority: 0.5,
+    },
+    {
+      url: 'https://retroloop.io/profile',
+      lastModified: new Date(),
+      changeFrequency: 'monthly',
+      priority: 0.5,
+    },
+  ]
+}
