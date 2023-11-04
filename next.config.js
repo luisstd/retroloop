4be023ea61dbcbfb802966abe109297ae3287bf0
Luis Steidle <luis@steidle.me>
2023-11-04 16:43:54 +0100

    chore: update sentry configuration

--- next.config.js
+++ next.config.js
@@ -14,10 +14,32 @@ const nextConfig = withPlausibleProxy()({
   },
 })
 
-module.exports = withSentryConfig(nextConfig, {
-  silent: true,
-  hideSourcemaps: true,
-  tunnel: '/sentry',
+const sentryConfig = {
+  // For all available options, see:
+  // https://github.com/getsentry/sentry-webpack-plugin#options
+  org: 'noisia',
+  project: 'retroloop',
   release: version,
   environment: process.env.NODE_ENV,
-})
+  silent: true,
+
+  // For all available options, see:
+  // https://docs.sentry.io/platforms/javascript/guides/nextjs/manual-setup/
+
+  // Upload a larger set of source maps for prettier stack traces (increases build time)
+  widenClientFileUpload: true,
+
+  // Transpiles SDK to be compatible with IE11 (increases bundle size)
+  transpileClientSDK: true,
+
+  // Routes browser requests to Sentry through a Next.js rewrite to circumvent ad-blockers (increases server load)
+  tunnelRoute: '/sentry',
+
+  // Hides source maps from generated client bundles
+  hideSourceMaps: true,
+
+  // Automatically tree-shake Sentry logger statements to reduce bundle size
+  disableLogger: true,
+}
+
+module.exports = withSentryConfig(nextConfig, sentryConfig)
--- sentry.server.config.ts
+++ sentry.server.config.ts
@@ -4,14 +4,12 @@
 
 import * as Sentry from '@sentry/nextjs'
 
-const SENTRY_DSN = process.env.SENTRY_DSN || process.env.NEXT_PUBLIC_SENTRY_DSN
-
 Sentry.init({
-  dsn: SENTRY_DSN,
+  dsn: 'https://a29eddcb3ff645a5b9c2be094f3f80f2@o4504548310843392.ingest.sentry.io/4504589688766464',
+
   // Adjust this value in production, or use tracesSampler for greater control
-  tracesSampleRate: 1.0,
-  // ...
-  // Note: if you want to override the automatic release value, do not set a
-  // `release` value here - use the environment variable `SENTRY_RELEASE`, so
-  // that it will also get attached to your source maps
+  tracesSampleRate: 1,
+
+  // Setting this option to true will print useful information to the console while you're setting up Sentry.
+  debug: false,
 })
