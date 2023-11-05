36450231819cbf4eb1bc824427cdf08f06322700
Luis Steidle <luis@steidle.me>
2023-11-05 01:47:55 +0100

    chore: update testing configs

--- vitest.config.ts
+++ vitest.config.ts
@@ -11,7 +11,7 @@ export default defineConfig({
       reporter: ['json'],
       reportsDirectory: './coverage',
     },
-    exclude: [...configDefaults.exclude, './tests'],
+    exclude: [...configDefaults.exclude, './src/tests'],
   },
   resolve: {
     alias: {
