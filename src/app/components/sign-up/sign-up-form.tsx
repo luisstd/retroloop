2202fc43673e42f1d39a8dbad345a60159c89ca8
Luis Steidle <luis@steidle.me>
2023-11-12 21:43:01 +0100

    refactor: use correct import for next router

--- src/app/components/sign-up/sign-up-form.tsx
+++ src/app/components/sign-up/sign-up-form.tsx
@@ -1,7 +1,7 @@
 'use client'
 
 import { Field, Form, Formik } from 'formik'
-import { useRouter } from 'next/router'
+import { useRouter } from 'next/navigation'
 import { useSession } from 'next-auth/react'
 
 import { Button } from '@/app/ui/button/button'
