2318eaebe734ecb96acc64c36226cf7ae221a8cd
Luis Steidle <luis@steidle.me>
2023-11-19 00:07:48 +0100

    chore: update sentry dsn variable

--- sentry.client.config.ts
+++ sentry.client.config.ts
@@ -1,8 +1,10 @@
 import { CaptureConsole, Debug, ExtraErrorData, HttpClient } from '@sentry/integrations'
 import * as Sentry from '@sentry/nextjs'
 
+const SENTRY_DSN = process.env.SENTRY_DSN || process.env.NEXT_PUBLIC_SENTRY_DSN
+
 Sentry.init({
-  dsn: process.env.SENTRY_DSN,
+  dsn: SENTRY_DSN,
   environment: process.env.NODE_ENV,
   tracesSampleRate: 1.0,
   profilesSampleRate: 1.0,
