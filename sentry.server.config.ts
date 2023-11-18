280b7e9d2a24f8d2041022294d44a7a6dc248593
Luis Steidle <luis@steidle.me>
2023-11-18 23:42:17 +0100

    chore: configure sentry integration

--- sentry.server.config.ts
+++ sentry.server.config.ts
@@ -1,8 +1,11 @@
 import * as Sentry from '@sentry/nextjs'
+import { ProfilingIntegration } from '@sentry/profiling-node'
 
 Sentry.init({
   dsn: process.env.SENTRY_DSN,
   environment: process.env.NODE_ENV,
-  tracesSampleRate: 1,
   debug: false,
+  tracesSampleRate: 1.0,
+  profilesSampleRate: 1.0,
+  integrations: [new ProfilingIntegration()],
 })
