171196f748671a719745d662dcfc40f18a1c8767
Luis Steidle <luis@steidle.me>
2023-11-18 23:12:18 +0100

    chore: configure sentry integrations

--- sentry.client.config.ts
+++ sentry.client.config.ts
@@ -3,10 +3,14 @@ import * as Sentry from '@sentry/nextjs'
 Sentry.init({
   dsn: process.env.SENTRY_DSN,
   environment: process.env.NODE_ENV,
-  tracesSampleRate: 1,
+  tracesSampleRate: 1.0,
   profilesSampleRate: 1.0,
   debug: false,
   replaysOnErrorSampleRate: 1.0,
   replaysSessionSampleRate: 0.1,
-  integrations: [new Sentry.Replay()],
+  integrations: [
+    new Sentry.BrowserTracing(),
+    new Sentry.BrowserProfilingIntegration(),
+    new Sentry.Replay(),
+  ],
 })
