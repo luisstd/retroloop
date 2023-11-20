17e068ff9f28074a936060d918c6c1fc92382d93
Luis Steidle <luis@steidle.me>
2023-11-20 22:46:24 +0100

    chore: use VERCEL_ENV for sentry

--- sentry.server.config.ts
+++ sentry.server.config.ts
@@ -6,7 +6,7 @@ const SENTRY_DSN = process.env.SENTRY_DSN || process.env.NEXT_PUBLIC_SENTRY_DSN
 
 Sentry.init({
   dsn: SENTRY_DSN,
-  environment: process.env.NODE_ENV,
+  environment: process.env.VERCEL_ENV,
   debug: false,
   tracesSampleRate: 1.0,
   profilesSampleRate: 1.0,
