d2957a199522cd7726b2667321983fc870b47df8
Luis Steidle <luis@steidle.me>
2024-02-11 20:23:42 +0100

    refactor: remove whitespace

--- src/app/dashboard/layout.tsx
+++ src/app/dashboard/layout.tsx
@@ -1,3 +1,3 @@
 export default async function DashboardLayout({ children }: { children: React.ReactNode }) {
-  return <section className='flex h-full w-full flex-col items-center  gap-10'>{children}</section>
+  return <section className='flex h-full w-full flex-col items-center gap-10'>{children}</section>
 }
