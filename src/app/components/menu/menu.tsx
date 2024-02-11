d543077615253373780f71155c2b99a30a183caf
Luis Steidle <luis@steidle.me>
2024-02-11 20:57:19 +0100

    refactor: set menu to full width on mobile

--- src/app/components/menu/menu.tsx
+++ src/app/components/menu/menu.tsx
@@ -24,56 +24,58 @@ export function Menu() {
   const isSignedUp = status === 'authenticated' && session?.user?.name !== null
 
   return (
-    <NavigationMenu className='m-5 flex items-center gap-2 rounded-full border-2 bg-background p-1 text-xl shadow-sm'>
-      <NavigationMenuList>
-        <NavigationMenuItem className='items-top flex cursor-pointer text-center text-2xl font-bold italic sm:text-3xl'>
-          <Link href='/' legacyBehavior passHref>
-            <NavigationMenuLink
-              className={cn(
-                navigationMenuTriggerStyle(),
-                'cursor-pointer rounded-l-3xl rounded-r-md'
-              )}
-              aria-label='Landingpage'
-            >
-              <IconInfinity className='self-center' size={28} />
-            </NavigationMenuLink>
-          </Link>
-        </NavigationMenuItem>
-      </NavigationMenuList>
+    <div className='min-w-full'>
+      <NavigationMenu className='m-5 flex max-w-none items-center gap-2 rounded-full border-2 bg-background p-1 text-xl shadow-sm md:mx-auto md:max-w-max'>
+        <NavigationMenuList>
+          <NavigationMenuItem className='items-top flex cursor-pointer text-center text-2xl font-bold italic sm:text-3xl'>
+            <Link href='/' legacyBehavior passHref>
+              <NavigationMenuLink
+                className={cn(
+                  navigationMenuTriggerStyle(),
+                  'cursor-pointer rounded-l-3xl rounded-r-md'
+                )}
+                aria-label='Landingpage'
+              >
+                <IconInfinity className='self-center' size={28} />
+              </NavigationMenuLink>
+            </Link>
+          </NavigationMenuItem>
+        </NavigationMenuList>
 
-      <NavigationMenuList className='hidden sm:flex'>
-        <NavigationMenuItem>
-          <Link href='/dashboard' legacyBehavior passHref>
-            <NavigationMenuLink
-              className={cn(navigationMenuTriggerStyle(), 'cursor-pointer')}
-              onClick={!isSignedUp ? () => signIn() : undefined}
-              aria-label='Dashboard'
-            >
-              Dashboard
-            </NavigationMenuLink>
-          </Link>
-        </NavigationMenuItem>
+        <NavigationMenuList className='hidden sm:flex'>
+          <NavigationMenuItem>
+            <Link href='/dashboard' legacyBehavior passHref>
+              <NavigationMenuLink
+                className={cn(navigationMenuTriggerStyle(), 'cursor-pointer')}
+                onClick={!isSignedUp ? () => signIn() : undefined}
+                aria-label='Dashboard'
+              >
+                Dashboard
+              </NavigationMenuLink>
+            </Link>
+          </NavigationMenuItem>
 
-        <NavigationMenuItem aria-label=''>
-          <Link href='/profile' legacyBehavior passHref>
-            <NavigationMenuLink
-              className={cn(navigationMenuTriggerStyle(), 'cursor-pointer')}
-              onClick={!isSignedUp ? () => signIn() : undefined}
-              aria-label='Profile'
-            >
-              Profile
-            </NavigationMenuLink>
-          </Link>
-        </NavigationMenuItem>
-      </NavigationMenuList>
+          <NavigationMenuItem aria-label=''>
+            <Link href='/profile' legacyBehavior passHref>
+              <NavigationMenuLink
+                className={cn(navigationMenuTriggerStyle(), 'cursor-pointer')}
+                onClick={!isSignedUp ? () => signIn() : undefined}
+                aria-label='Profile'
+              >
+                Profile
+              </NavigationMenuLink>
+            </Link>
+          </NavigationMenuItem>
+        </NavigationMenuList>
 
-      <div>
-        <NavigationDropdown />
-        <ThemeDropdown />
-        {session?.user ? <UserDropdown /> : <LoginButton />}
-      </div>
+        <div>
+          <NavigationDropdown />
+          <ThemeDropdown />
+          {session?.user ? <UserDropdown /> : <LoginButton />}
+        </div>
 
-      <NavigationMenuViewport />
-    </NavigationMenu>
+        <NavigationMenuViewport />
+      </NavigationMenu>
+    </div>
   )
 }
