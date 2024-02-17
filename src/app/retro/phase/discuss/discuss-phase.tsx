23516ad835e492fe2f4a9c8538abd8a46d22a10e
Luis Steidle <luis@steidle.me>
2024-02-17 22:12:30 +0100

    feat: implement carousel for feedback items

--- src/app/retro/phase/discuss/discuss-phase.tsx
+++ src/app/retro/phase/discuss/discuss-phase.tsx
@@ -1,50 +1,59 @@
 'use client'
 
 import { Retrospective } from '@prisma/client'
-import { useState } from 'react'
 
-import { ItemStack } from '@/app/retro/phase/discuss/components/item-stack/item-stack'
-import { ItemSwitcher } from '@/app/retro/phase/discuss/components/item-switcher/item-switcher'
+import { Loader } from '@/app/components/loader/loader'
+import { Badge } from '@/app/ui/badge/badge'
+import { Card, CardDescription, CardTitle } from '@/app/ui/card/card'
+import {
+  Carousel,
+  CarouselContent,
+  CarouselItem,
+  CarouselNext,
+  CarouselPrevious,
+} from '@/app/ui/carousel/carousel'
 import { api } from '@/trpc/react'
+import { getFeedbackItemType } from '@/utils/utils'
 
 type DiscussPhaseProps = {
   selectedRetro: Retrospective
 }
 
 export function DiscussPhase({ selectedRetro }: DiscussPhaseProps) {
-  const retroItems = api.retroItem.getAllByRetroId.useQuery(selectedRetro.id)
-
-  const [currentIndex, setCurrentIndex] = useState(0)
-
-  const handleNextItem = () => {
-    if (retroItems.data && currentIndex > currentIndex - retroItems.data.length) {
-      setCurrentIndex(currentIndex + 1)
-    }
-  }
-
-  const handlePreviousItem = () => {
-    if (retroItems.data && currentIndex > currentIndex - retroItems.data.length) {
-      setCurrentIndex(currentIndex - 1)
-    }
-  }
+  const { data: feedbackItems, isLoading } = api.retroItem.getAllByRetroId.useQuery(
+    selectedRetro.id
+  )
 
   return (
-    <>
-      {retroItems.data && (
-        <>
-          <div className='col-span-2 row-start-4 h-full w-[100rem] min-w-full max-w-full px-2 lg:min-h-screen'>
-            <ItemStack retroItems={retroItems.data} currentIndex={currentIndex} />
-          </div>
-          <div className='col-span-2 row-start-4 h-full w-[100rem] min-w-full max-w-full px-2 lg:min-h-screen'>
-            <ItemSwitcher
-              handleNextItem={handleNextItem}
-              handlePreviousItem={handlePreviousItem}
-              currentIndex={currentIndex}
-              retroItems={retroItems.data}
-            />
-          </div>
-        </>
-      )}
-    </>
+    <div className='col-start-2 w-full lg:min-h-screen'>
+      <Carousel className='lg:mt-48'>
+        <CarouselContent>
+          {feedbackItems ? (
+            feedbackItems
+              .sort((a, b) => b.votes - a.votes)
+              .map((item, index) => (
+                <CarouselItem key={item.id}>
+                  <Card className='mx-auto flex min-h-[18rem] w-11/12 flex-col justify-between break-words p-5'>
+                    <CardTitle className='flex flex-row items-center justify-between'>
+                      Feedback <Badge variant='secondary'>{getFeedbackItemType(item.type)}</Badge>
+                    </CardTitle>
+                    <CardDescription className='prose text-lg'>{item.content}</CardDescription>
+                    <div className='flex flex-row items-center justify-between'>
+                      <Badge className='text-lg font-bold'>+{item.votes}</Badge>
+                      <CardDescription className='self-end justify-self-end px-5 text-lg'>
+                        {`${index + 1}/${feedbackItems.length}`}
+                      </CardDescription>
+                    </div>
+                  </Card>
+                </CarouselItem>
+              ))
+          ) : (
+            <Loader isLoading={isLoading} />
+          )}
+        </CarouselContent>
+        <CarouselPrevious />
+        <CarouselNext />
+      </Carousel>
+    </div>
   )
 }
