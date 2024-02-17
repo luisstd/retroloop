807bc1190cadcc04009859645bfcab597970a437
Luis Steidle <luis@steidle.me>
2024-02-17 22:23:09 +0100

    refactor: set correct width for layout container

--- src/app/retro/layout.tsx
+++ src/app/retro/layout.tsx
@@ -2,7 +2,7 @@ import { Card } from '@/app/ui/card/card'
 
 export default function RetroLayout({ children }: { children: React.ReactNode }) {
   return (
-    <Card className='m-5 flex w-full flex-col place-items-center gap-5 p-5 lg:grid lg:grid-cols-3'>
+    <Card className='m-5 flex w-[calc(100%-1rem)] flex-col place-items-center gap-5 p-5 lg:grid lg:grid-cols-3'>
       {children}
     </Card>
   )
