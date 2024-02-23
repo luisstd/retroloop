1f4a0859123d7aa5a4e8dc8bdc691b79014c3d21
Luis Steidle <luis@steidle.me>
2024-02-23 21:11:33 +0100

    chore: clean up db schema

---
+++ prisma/migrations/20240223200800_schema_cleanup/migration.sql
@@ -0,0 +1,8 @@
+/*
+  Warnings:
+
+  - You are about to drop the column `itemCollectionId` on the `RetroItem` table. All the data in the column will be lost.
+
+*/
+-- AlterTable
+ALTER TABLE "RetroItem" DROP COLUMN "itemCollectionId";
