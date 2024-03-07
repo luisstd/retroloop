10f44d45fa28156ac80a4cea2e9503cf069d6190
Luis Steidle <luis@steidle.me>
2024-03-07 22:46:47 +0100

    chore: use NODE_ENV as release environment

--- sentry.client.config.ts
+++ sentry.client.config.ts
@@ -10,7 +10,7 @@ const SENTRY_DSN = process.env.SENTRY_DSN || process.env.NEXT_PUBLIC_SENTRY_DSN
 
 Sentry.init({
   dsn: SENTRY_DSN,
-  environment: process.env.VERCEL_ENV,
+  environment: process.env.NODE_ENV,
   tracesSampleRate: 1.0,
   profilesSampleRate: 1.0,
   debug: false,
