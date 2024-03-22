fadf529c9c060e0b5969cd0adac56bc0ad5c4e90
Luis Steidle <luis@steidle.me>
2024-03-22 17:28:09 +0100

    refactor: use NextResponse

--- src/app/api/ably/route.ts
+++ src/app/api/ably/route.ts
@@ -1,7 +1,8 @@
 import Ably from 'ably/promises'
+import { NextResponse } from 'next/server'
 
 export async function GET() {
   const client = new Ably.Realtime(process.env.ABLY_API_KEY as string)
   const tokenRequestData = await client.auth.createTokenRequest({ clientId: 'retroloop' })
-  return Response.json(tokenRequestData)
+  return NextResponse.json(tokenRequestData)
 }
