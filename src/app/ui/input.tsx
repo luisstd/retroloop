e9ae0488c863dd37b6dd27f3de2507fb6f634ca2
Luis Steidle <luis@steidle.me>
2025-10-18 15:44:00 +0200

    refactor: update input & label components

--- src/app/ui/input.tsx
+++ src/app/ui/input.tsx
@@ -1,25 +1,25 @@
-import * as React from 'react'
+import React, { InputHTMLAttributes } from "react";
 
-import { cn } from '@/utils/cn'
+interface InputProps extends InputHTMLAttributes<HTMLInputElement> {
+  className?: string;
+}
 
-export interface InputProps
-  extends React.InputHTMLAttributes<HTMLInputElement> {}
-
-const Input = React.forwardRef<HTMLInputElement, InputProps>(
-  ({ className, type, ...props }, ref) => {
-    return (
-      <input
-        type={type}
-        className={cn(
-          'border-input bg-background ring-offset-background placeholder:text-muted-foreground focus-visible:ring-ring flex h-10 w-full rounded-md border px-3 py-2 text-sm file:border-0 file:bg-transparent file:text-sm file:font-medium focus-visible:ring-2 focus-visible:ring-offset-2 focus-visible:outline-hidden disabled:cursor-not-allowed disabled:opacity-50',
-          className,
-        )}
-        ref={ref}
-        {...props}
-      />
-    )
-  },
-)
-Input.displayName = 'Input'
-
-export { Input }
+export const Input: React.FC<InputProps> = ({
+  type = "text",
+  placeholder = "Enter text",
+  className = "",
+  ...props
+}) => {
+  return (
+    <input
+      type={type}
+      placeholder={placeholder}
+      className={`px-4 py-2 w-full rounded border-2 shadow-md transition focus:outline-hidden focus:shadow-xs ${
+        props["aria-invalid"]
+          ? "border-destructive text-destructive shadow-xs shadow-destructive"
+          : ""
+      } ${className}`}
+      {...props}
+    />
+  );
+};
