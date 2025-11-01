3a4433fef5802e0803b90a25468c30d6d8d1c12c
Luis Steidle <luis@steidle.me>
2025-11-01 12:05:04 +0100

    Revert "chore: disable google oauth"
    This reverts commit 29ac28e510ebbc0516f2872de94dcd5df15531b3.

--- src/lib/auth.ts
+++ src/lib/auth.ts
@@ -49,6 +49,10 @@ export const auth = betterAuth({
       clientId: env.GITHUB_ID,
       clientSecret: env.GITHUB_SECRET as string,
     },
+    google: {
+      clientId: env.GOOGLE_CLIENT_ID,
+      clientSecret: env.GOOGLE_CLIENT_SECRET as string,
+    },
   },
   plugins: [
     magicLink({
